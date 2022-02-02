2de943ed44967252c371e37999ca574cf1e635ad
Андрей Мухин <andreymuhin02@gmail.com>
2022-02-02 10:58:51 +0300

    Доделала

--- SimUDuck/ClassLibrary1/Duck.cs
+++ SimUDuck/ClassLibrary1/Duck.cs
@@ -8,9 +8,8 @@ namespace SimUDuck
 {
     public abstract class Duck : FlyBehavior, QuackBehavior
     {
-        public QuackBehavior quackBehavior { get; set; }
-        protected FlyBehavior flyBehavior { get; set; }
-
+        protected  QuackBehavior quackBehavior;
+        protected FlyBehavior flyBehavior;
         public string swim()
         {
             return "Буль буль буль" + this.GetType();
@@ -24,6 +23,17 @@ namespace SimUDuck
         public string performFly()
         {
             return flyBehavior.Fly
+        }
+        public FlyBehavior SetFlyBehavior(FlyBehavior flyBehavior)
+        {
+            this.flyBehavior = flyBehavior;
+            return flyBehavior;
+        }
+
+        public QuackBehavior SetQuackBehavior(QuackBehavior quackBehavior)
+        {
+            this.quackBehavior = quackBehavior;
+            return quackBehavior;
         }
     }
 
